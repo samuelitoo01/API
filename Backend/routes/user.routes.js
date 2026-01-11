6e356919bd9b25ddbfb0a40bc5d7fddcac85cfe8
Samuel Sarmiento <samuelsarmiento@MacBook-Air-de-Samuel.local>
2026-01-11 15:33:59 -0500

    CRUD terminado

--- Backend/routes/user.routes.js
+++ Backend/routes/user.routes.js
@@ -1,9 +1,11 @@
 import { Router } from "express";
-import { getusuarios , postUsuarios , eliminarUsuarioController} from "../controllers/user.controller.js";
+import { getusuarios , postUsuarios , eliminarUsuarioController , actualizarUsuarioController} from "../controllers/user.controller.js";
+
 
 const router = Router(); 
 
 router.get('/obtener' , getusuarios);
 router.post('/obtener' , postUsuarios);
 router.delete('/obtener/:id' , eliminarUsuarioController )
+router.put('/obtener/:id' , actualizarUsuarioController)
 export default router
