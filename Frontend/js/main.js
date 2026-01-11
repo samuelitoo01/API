0bfa76ec0e75554a20b88d13a4c05eb13fcf2190
Samuel Sarmiento <samuelsarmiento@MacBook-Air-de-Samuel.local>
2026-01-11 15:36:03 -0500

    Frontend termiando , y conectado correctamente con el backend

--- Frontend/js/main.js
+++ Frontend/js/main.js
@@ -1,8 +1,9 @@
 // Este es el archivo principal a donde se importaran todos los modulos del frontend 
-import { addDataStudent, renderizarDatosBd , eliminarDatos } from "./ui.js"; 
+import { addDataStudent, renderizarDatosBd , eliminarDatos , actualizarUsuario } from "./ui.js"; 
 // Funcion para obtener los datos de ui (interfaz de usuario) y enviarlos por medio del fetch 
 // al backend para que los agregue a la base de datos
 
 addDataStudent();
 renderizarDatosBd()
 eliminarDatos()
+actualizarUsuario()
